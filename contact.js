e5a9186386cd4645069802704348751ceed715cf
Carly Zimmerman <zimmermancarly00@gmail.com>
2024-08-04 14:04:19 -0600

    added formspree and adjusted js to work with formspree

--- contact.js
+++ contact.js
@@ -1,8 +1,30 @@
 const form = document.querySelector("#form");
 const submitButton = document.querySelector("#submit");
 
-form.addEventListener("submit", (e) => {
-  submitButton.disabled = true;
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  window.location.href = window.location.origin + "/success.html";
+  submitButton.disabled = true;
+
+  const formData = new FormData(form);
+  try {
+    const response = await fetch(form.action, {
+      method: form.method,
+      headers: {
+        Accept: "application/json",
+      },
+      body: formData,
+    });
+
+    if (response.ok) {
+      window.location.href = window.location.origin + "/success.html";
+    } else {
+      console.error("Form submission error:", response.statusText);
+      alert("There was an issue with your submission. Please try again.");
+      submitButton.disabled = false;
+    }
+  } catch (error) {
+    console.error("Form submission error:", error);
+    alert("There was an issue with your submission. Please try again.");
+    submitButton.disabled = false;
+  }
 });
